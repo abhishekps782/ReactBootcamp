d01bbe45d927cddb95bfc43445ce58d70e9c1100
Abhishek Anand <abhishekps782@gmail.com>
2022-11-22 21:21:30 +0530

    use import and remove cdn script sources

--- 0_HelloWorld/src/app.js
+++ 0_HelloWorld/src/app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 const heading = React.createElement("h1", {}, "Hello World");
 rootElement.render(heading);
